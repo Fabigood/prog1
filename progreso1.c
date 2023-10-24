e14179a1b440834b55f6cc047a7a9904edd72f85
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-10-24 14:53:56 -0500

    paso 2

--- progreso1.c
+++ progreso1.c
@@ -5,10 +5,12 @@ int num;
 
 printf("Inserte un numero de 2 cifras entero: "); //pedimos el numero al usuario
 scanf("%d", &num);
+int sumaDigitos(int num){
+        int suma =0;                    //agregamsop un while para poder descartar numeros menores de 0
+    
+    
 
-
-
-
+printf("El numero es : %d\n", num);
 
 return 0;
 }

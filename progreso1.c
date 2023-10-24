1488149c9e39b62590c8306438f3a460ab37dab9
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-10-24 14:57:55 -0500

    paso 3

--- progreso1.c
+++ progreso1.c
@@ -7,7 +7,10 @@ printf("Inserte un numero de 2 cifras entero: "); //pedimos el numero al usuario
 scanf("%d", &num);
 int sumaDigitos(int num){
         int suma =0;                    //agregamsop un while para poder descartar numeros menores de 0
-    
+    while (num > 0) {   //con al division de mod saco el numero divido a 10 con eso recorro
+        
+
+        
     
 
 printf("El numero es : %d\n", num);
